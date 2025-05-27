92d5a659e9dc32d390a88445b65e43b300a2bb62
Andres Subero <andressuberoh@gmail.com>
2025-05-27 20:22:21 -0300

    second commit

---
+++ app/src/main/java/com/example/tp3_petshop/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.tp3_petshop
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tp3_petshop.ui.theme.TP3PETSHOPTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tp3_petshop.views.DetailView
+import com.example.tp3_petshop.views.HomeView
+import com.example.tp3_petshop.views.SplashView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TP3PETSHOPTheme {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "initial") {
+                    composable("initial") {
+                        SplashView(
+                            //onLoginClick = { navController.navigate("login") },
+                            //onRegisterClick = { navController.navigate("register") }
+
+                        )
+                    }
+                    composable("home") {
+                        HomeView()
+                    }
+                    composable("detail") {
+                        DetailView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP3PETSHOPTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/tp3_petshop/views/Detail.kt
@@ -0,0 +1,48 @@
+package com.example.tp3_petshop.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+import com.example.tp3_petshop.ui.theme.TP3PETSHOPTheme
+
+@Composable
+fun DetailView() {
+    Scaffold() { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                text = "Detail",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge,
+                textAlign = TextAlign.Center,
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+
+fun DetailViewPreview () {
+    TP3PETSHOPTheme (darkTheme = false, dynamicColor = false) {
+        DetailView()
+    }
+}

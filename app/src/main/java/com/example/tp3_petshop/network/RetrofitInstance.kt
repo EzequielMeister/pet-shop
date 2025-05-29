557ae159c56cc52dd51cb219385f3bca8857a48f
NimajF <benjafran2000@gmail.com>
2025-05-29 01:37:12 -0300

    Add retrofit service and repository

---
+++ app/src/main/java/com/example/tp3_petshop/network/RetrofitInstance.kt
@@ -0,0 +1,28 @@
+package com.example.tp3_petshop.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    private const val BASE_URL = "https://dummyjson.com/"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    } // opcional: para ver los logs de las peticiones HTTP (fran)
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
+        .build()
+
+    val api: ProductService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ProductService::class.java)
+    }
+}

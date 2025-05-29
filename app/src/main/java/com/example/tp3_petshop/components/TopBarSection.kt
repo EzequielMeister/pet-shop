81aa650bdf3676215651298e8ed3eeaa85d2799e
NimajF <benjafran2000@gmail.com>
2025-05-29 01:38:30 -0300

    Add TopBarSection

---
+++ app/src/main/java/com/example/tp3_petshop/components/TopBarSection.kt
@@ -0,0 +1,68 @@
+package com.example.tp3_petshop.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TopBarSection(
+    title: String,
+    showFavorite: Boolean = false,
+    onBackClick: () -> Unit,
+    onFavoriteClick: () -> Unit = {}
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = onBackClick,
+                modifier = Modifier
+                    .size(46.dp)
+                    .background(Color.White, shape = CircleShape)
+            ) {
+                Icon(Icons.Default.ArrowBack, contentDescription = "Back")
+            }
+
+            Spacer(modifier = Modifier.width(46.dp)) // Para alinear visualmente
+        }
+
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold
+        )
+
+        if (showFavorite) {
+            IconButton(
+                onClick = onFavoriteClick,
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .size(46.dp)
+                    .background(Color.White, shape = CircleShape)
+            ) {
+                Icon(
+                    Icons.Default.FavoriteBorder,
+                    contentDescription = "Favorite"
+                )
+            }
+        }
+    }
+}
